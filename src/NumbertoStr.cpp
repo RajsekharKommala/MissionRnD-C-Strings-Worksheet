08a0fb2dc9689663dd0cc9752b75fe651572b48b
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-14 23:26:23 -0700

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,44 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
-
-
 void number_to_str(float number, char *str,int afterdecimal){
-	
+	int  decimal_number = 1, i, j = 0, k = 0;
+	int len = afterdecimal;
+	for (i = 1; i <= len; i++)
+		decimal_number *= 10;
+
+	if (number < 0){
+		str[j++] = '-';
+		k = 1;
+		number *= -1;
+	}
+	//extract the integer part
+	int i_part = (int)number;
+	int c;
+	c = number * decimal_number;
+	i = 0;
+	while (i<len)
+	{
+		i++;
+		str[j++] = (c % 10) + 48;
+		c /= 10;
+	}
+	if (len>0){
+		str[j++] = '.';
+	}
+	printf("%d ",i_part);
+	while (i_part != 0)
+	{
+		str[j++] = (i_part % 10) + 48;
+		i_part /= 10;
+	}
+	str[j] = '\0';
+	printf("\n %s", str);
+	int n;
+	while (k < j){
+		n = str[--j];
+		str[j] = str[k];
+		str[k++] = n;
+	}
+
 }
