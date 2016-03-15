9ae1d47114ab319f73681259948e30c4587e8f49
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-14 23:35:48 -0700

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -12,5 +12,25 @@ NOTES: Don't create new string.
 */
 
 char removeSpaces(char *str) {
+	if (str == nullptr)
+		return '\0';
+	int i = 0, j = 0, k = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			j = i + 1;
+			while (str[j] == ' ')
+				j++;
+			k = i;
+			while (str[j] != '\0')
+			{
+				str[k] = str[j];
+				j++, k++;
+
+			}
+			str[k] = '\0';
+		}
+	}
 	return '\0';
 }
