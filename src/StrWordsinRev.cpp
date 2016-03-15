3d2defded2b09759159826685c459a4d1dd1b467
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-14 23:34:28 -0700

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -13,5 +13,33 @@ NOTES: Don't create new string.
 #include <string.h>
 
 void str_words_in_rev(char *input, int len){
-	
+	if (input == nullptr&&len <= 0)
+		return;
+	int i, j, k;
+	char ch;
+	i = 0;
+	j = len - 1;
+	while (i<j){
+		ch = input[i];
+		input[i] = input[j];
+		input[j] = ch;
+		i++, j--;
+	}
+	for (i = 0; input[i] != '\0'; i++)
+	{
+		k = i;
+		while (input[k] != ' '&&input[k] != '\0')
+			k++;
+
+		j = k - 1;
+		while (i<j){
+			ch = input[i];
+			input[i] = input[j];
+			input[j] = ch;
+			i++, j--;
+		}
+		i = k;
+	}
+
+
 }
